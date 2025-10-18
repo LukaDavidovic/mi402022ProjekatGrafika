21dff6a471c978cc9816c9e69313d4c0be249ace
matfrg <luka.davidovic5@gmail.com>
2025-10-18 20:05:07 +0200

    Create MyApp and i found my model from internet to add in future commits(i add model in test dir)

---
+++ app/src/MyApp.cpp
@@ -0,0 +1,12 @@
+//
+// Created by matfrg on 10/18/25.
+//
+
+//#include "../include/MyApp.hpp"
+
+#include <MyApp.hpp>
+#include <spdlog/spdlog.h>
+
+namespace app {
+void MyApp::app_setup() { spdlog::info("App setup complited!"); }
+}// app
