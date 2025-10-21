17e924dad8ff451f14b943d931bf0dcfa11561b6
matfrg <luka.davidovic5@gmail.com>
2025-10-21 19:58:13 +0200

    update: Garage shader has direction and point light now.

--- app/resources/shaders/garage.glsl
+++ app/resources/shaders/garage.glsl
@@ -35,36 +35,43 @@ void main()
 
 out vec4 FragColor;
 
-
 in vec2 TexCoords;
 in vec3 Normal;
 in vec3 FragPos;
 
 uniform sampler2D texture_diffuse1;
 
+
+
 // pozicija svetla
+
 uniform vec3 lightPos1;
+uniform vec3 lightDir1;
 
-uniform vec3 viewPos;      // kamera pozicija
+uniform vec3 viewPos;
 
-uniform vec3 lightColor1;   // belo
+uniform vec3 lightColor1;
+uniform vec3 lightColor2;
+
+uniform float lightIntensity;//ovo svetlo ce moci da se menja
+
+uniform float constantAttenuation;
+uniform float linearAttenuation;
+uniform float quadraticAttenuation;
 
 
 void main()
 {
-
     vec3 norm = normalize(Normal);
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 textureColor = texture(texture_diffuse1, TexCoords).rgb;
 
-    //f-ja za jedno svetlo
+    float ambientStrength = 0.3;
+    float specularStrength = 0.3;
 
-    float ambientStrength = 0.7;
-    float specularStrength = 0.7;
+    vec3 totalLight = vec3(0.05);
 
-    vec3 totalLight = vec3(0.1);
-
-    // svetlo1
+    // svetlo1 - point light(lightcolor1,lightPos1)
 
     {
         vec3 lightDir = normalize(lightPos1 - FragPos);
@@ -75,6 +82,22 @@ void main()
         vec3 ambient = ambientStrength * lightColor1;
         vec3 diffuse = diff * lightColor1;
         vec3 specular = specularStrength * spec * lightColor1;
+
+        float distance = length(lightPos1 - FragPos);
+        float attenuation = 1.0 / (constantAttenuation + linearAttenuation * distance + quadraticAttenuation * distance * distance);
+
+        totalLight += (ambient + diffuse + specular) * lightIntensity * attenuation;
+    }
+    // svetlo 2 direkciono
+    {
+        vec3 lightDir = normalize(-lightDir1);
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec3 reflectDir = reflect(-lightDir, norm);
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+
+        vec3 ambient = ambientStrength * lightColor2;
+        vec3 diffuse = diff * lightColor2;
+        vec3 specular = specularStrength * spec * lightColor2;
 
         totalLight += (ambient + diffuse + specular);
     }
